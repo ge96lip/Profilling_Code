e3214a5aaf538d16a8bad86549c6a756ffc9effb
phoebe <phoebes@kth.se>
2025-01-21 15:42:09 +0100

    JuliaSet updates. new mprofile figure with timing labels.

--- JuliaSet.py
+++ JuliaSet.py
@@ -60,15 +60,18 @@ def calc_pure_python(desired_width, max_iterations):
 @profile
 def calculate_z_serial_purepython(maxiter, zs, cs):
     """Calculate output list using Julia update rule"""
-    output = [0] * len(zs)
-    for i in range(len(zs)):
-        n = 0
-        z = zs[i]
-        c = cs[i]
-        while abs(z) < 2 and n < maxiter:
-            z = z * z + c
-            n += 1
-        output[i] = n
+    with profile.timestamp("create_output_list"):
+        output = [0] * len(zs)
+    time.sleep(1)
+    with profile.timestamp("calculate_output"):
+        for i in range(len(zs)):
+            n = 0
+            z = zs[i]
+            c = cs[i]
+            while abs(z) < 2 and n < maxiter:
+                z = z * z + c
+                n += 1
+            output[i] = n
     return output
 
 if __name__ == "__main__":
